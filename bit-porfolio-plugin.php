0f0ce0f861298d0855042318b442b27d4c27469a
motiejus1 <motiejus.baliukonis@vka.lt>
2022-04-07 19:45:21 +0300

    *customizer settings: text, image, colors

--- bit-porfolio-plugin.php
+++ bit-porfolio-plugin.php
@@ -45,6 +45,7 @@ function bit_portfolio_customize_background_color($wp_customize) {
         'priority' => 100
     ));
 
+    //Kad cia yra sukuriamas tekstinis laukas
     $wp_customize->add_setting('bit_portofolio_background_color', array(
         'default' => 'red',
         'sanitize_callback' => ''
@@ -56,6 +57,32 @@ function bit_portfolio_customize_background_color($wp_customize) {
         'section' => 'bit_portfolio_bit_colors',
         'type' => 'text',
         'priority' => 10
+    )));
+
+    //sukursime nauja nustatyma kuriame galime pasirinkti spalva
+
+    $wp_customize->add_setting('bit_portfolio_second_background_color', array(
+        'default' => '#FFFFFF',
+        'sanitize_callback' => ''
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize,'bit_portfolio_second_background_color', array(
+        'label' => 'Second background color',
+        'section' => 'bit_portfolio_bit_colors',
+        'settings' => 'bit_portfolio_second_background_color'
+    )));
+
+    //Kuriame nuotraukos iterpimo lauka
+
+    $wp_customize->add_setting('bit_portfolio_custom_image', array(
+        'default' => '',
+        'sanitize_callback' => ''
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'bit_portfolio_custom_image', array(
+        'label'=>'Custom Image',
+        'description' => 'Select your image',
+        'section' => 'bit_portfolio_bit_colors'
     )));
 
 }
